55802594b2b26c2eabe876a31d82e1e14b2a445d
Aygün Bayır <turkoaygun@gmail.com>
2024-10-02 00:04:31 +0300

    Fluent Validation

--- Services/Extensions/ServiceExtensions.cs
+++ Services/Extensions/ServiceExtensions.cs
@@ -1,8 +1,9 @@
-﻿using App.Repositories.Products;
-using App.Repositories;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using App.Services.Products;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using System.Reflection;
 
 namespace App.Services.Extensions
 {
@@ -11,6 +12,9 @@ namespace App.Services.Extensions
         public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddScoped<IProductService, ProductService>();
+
+            services.AddFluentValidationAutoValidation();
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
 
             return services;
         }
